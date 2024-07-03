55f1cca97db66c9ea4f4dc8aa69c718e582be195
mustafaeisa <mustafafaroug77@gmail.com>
2024-07-03 14:49:33 -0700

    first comt

--- components/caring.jsx
+++ components/caring.jsx
@@ -7,14 +7,13 @@ const Caring = () => {
         <h1 className=' text-xl font-bold mb-2'>Manage your entire community <br/> in a single system</h1>
         <h2 className='w-2/5 text-center mx-auto'>The Nexcent blog is the best place to read about the latest membership insights,
         <br/>  trends and more. See who's joining the community,
-             read about how our community are increasing their membership income and lot's more.​
-        </h2>
-        {/* <div className='py-6 flex justify-around mt-10 flex-col lg:flex-row items-center'>
+             read about how our community are increasing their membership income and lot's more. </h2>
+        <div className='py-6 flex justify-around mt-10 flex-col lg:flex-row items-center'>
             <Card isImg={false} txt={`Creating Streamlined Safeguarding Processes with OneRen`}/>
             <Card isImg={false} txt={`What are your safeguarding responsibilities and how can you manage them?`}/>
             <Card isImg={false} txt={`What are your safeguarding responsibilities and how can you manage them`}/>
           
-        </div> */}
+        </div>
     </div>
   )
 }
